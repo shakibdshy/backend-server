a5301b1cd9189f890db5df2ac5982fbaf135c32c
Md Habibur Rahman <shakibdshy@gmail.com>
2022-09-04 16:27:42 +0600

    Update Cors

--- index.js
+++ index.js
@@ -22,7 +22,13 @@ const connect = () => {
     })
 }
 
-app.use(cors());
+const corsOptions = {
+    origin: '*',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRouter);
